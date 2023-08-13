ce8507c0417b121d74b62fa7db59e44c1d5980f3
Shashank Mokashi <shashankmokashi9819@gmail.com>
2023-08-13 11:45:27 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -19,9 +19,9 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='Krish',
-author_email='krishnaik06@gmail.com',
+author='Shashank',
+author_email='shashankmokashi9819@gmailcom',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
-)
+) 
